6fbbd965c95311d44436fd3c9b8972afca4073c2
SVidu96 <supunviduranga96@gmail.com>
2025-08-14 13:38:47 -0500

    configured auth0 added Role based authorization

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,41 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS services
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowedOriginsPolicy",
+        policy => policy.WithOrigins(allowedOrigins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["Authentication:Authority"];
+    options.Audience = builder.Configuration["Authentication:Audience"];
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "sub",
+        RoleClaimType = "permissions"
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdmin", policy => policy.RequireRole("admin"));
+    options.AddPolicy("RequireBusinessManager", policy => policy.RequireRole("business-manager"));
+    options.AddPolicy("RequireCustomer", policy => policy.RequireRole("customer"));
+});
 
 var app = builder.Build();
 
@@ -18,8 +56,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowedOriginsPolicy");
 
 app.Run();
